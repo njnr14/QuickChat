60fd88b6ba8c5ed32c1ad4e2692fc014c90e06dd
Neeraj Rathore <njratnr14@gmail.com>
2025-02-19 23:54:38 +0530

    project initialization and backend implementation start here

---
+++ Backend/src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import authRoute from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+
+const app = express()
+const PORT = process.env.PORT;
+
+app.use("/api/auth", authRoute);
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`)
+    connectDB();
+
+})  
