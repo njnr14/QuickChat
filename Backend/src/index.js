4f501743eb6df3f82fc77222452b94dedc3862eb
Neeraj Rathore <njratnr14@gmail.com>
2025-02-24 18:12:48 +0530

    backend initial phase complete

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,15 +2,19 @@ import express from "express";
 import authRoute from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+
 
 dotenv.config();
 
 
 const app = express()
+app.use(cookieParser());
 app.use(express.json());
 const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoute);
+app.use("/api/message", messageRoute);
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(PORT, () => {
