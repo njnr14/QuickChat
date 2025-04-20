ca4d9ba839845fe6f7381c3a6761799fd4eb1b5d
Neeraj Rathore <njratnr14@gmail.com>
2025-04-20 19:43:02 +0530

    deployment bug fix

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,41 +1,42 @@
 import express from "express";
-import authRoute from "./routes/auth.route.js";
 import dotenv from "dotenv";
-import messageRoute from "./routes/message.route.js";
-import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import {io , server,app} from "./lib/socket.js"
+
 import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-
-
-app.use(cookieParser());
-app.use(express.json());
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true, 
-}))
-
 const PORT = process.env.PORT;
-const __dirname = path.resolve("../");
+const __dirname = path.resolve();
 
-app.use("/api/auth", authRoute);
-app.use("/api/messages", messageRoute);
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-// app.get('/', (req, res) => res.send('Hello World!'))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname , "../Frontend/dist")));
-    app.get("*" , (req,res)=>{
-        res.sendFile(path.join(__dirname , "../Frontend" , "dist" , "index.html"))
-    })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+  });
 }
 
 server.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}!`)
-    connectDB();
-})  
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
